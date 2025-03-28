64e9f61270ef5ae6a495a2bc1ba9a205b81d2a4a
Tanieska Aguirre <tanieska98@gmail.com>
2025-03-27 21:13:28 -0600

    validate login

--- src/router/index.js
+++ src/router/index.js
@@ -47,4 +47,22 @@ const router = createRouter({
   ],
 })
 
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const token = localStorage.getItem('login')
+    if (token) {
+      // User is authenticated, proceed to the route
+      next()
+    } else {
+      // User is not authenticated, redirect to login
+      next('/login')
+    }
+  } else {
+    // Non-protected route, allow access
+    next()
+  }
+})
+
+
+
 export default router
